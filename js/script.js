98c4d7e513726513808ef22c4498f0b8e622fb03
Sununiqe <118557408+Sununiqe@users.noreply.github.com>
2023-02-24 22:57:24 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 let loginButton = document.querySelector('.login-button');
 let loginPopUp = document.querySelector('.login-popup');
+let closeButton = document.querySelector('.popup-close');
 
 loginButton.addEventListener('click', showPopUp);
+closeButton.addEventListener('click', close);
 
 function showPopUp(evt){
 
@@ -11,3 +13,13 @@ loginPopUp.classList.toggle('show-popup');
 };
 
 
+function close(){
+    loginPopUp.classList.remove('show-popup')
+}
+
+loginPopUp.addEventListener('click', (e) => {
+    if (e.target.classList.contains('show-popup')){
+        close();
+    }
+
+})
